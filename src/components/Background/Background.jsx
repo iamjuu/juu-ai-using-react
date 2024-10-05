86ffd165d94e6c8f1e9bbf8f7ed7d0460cf4fd6e
muhammed ajmal <muhammedajmalcc6424094@gmail.com>
2024-10-05 13:35:16 +0530

    animation text added

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BackgroundLines } from '../Ui/Background/BackGroundUi';
+// import {TextRevealCardPreview} from "../../components/Text/TextUi"
+import  Cat from "../../assets/cat/cat.png"
+import {TextRevealCard,
 
+} from "../../components/Ui/Text/TextUi"
 export function BackgroundLinesDemo() {
   return (
-      <BackgroundLines className=" bg-red-300 h-screen  md:h-screen text-white flex flex-col">
-  <h2 className="text-center text-2xl  sm:text-3xl md:text-4xl lg:text-5xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
-    hello, <br /> All.
-  </h2>
-  <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
-    Get the best advice from our experts, including expert artists,
-    painters, marathon enthusiasts and RDX, totally free.
-  </p>
+      <BackgroundLines className=" bg-black h-screen  justify-center items-center  md:h-screen text-white flex flex-col">
+<img className="w-40 hover:scale-110 transition-transform duration-300" src={Cat} alt="A cat" />
+<TextRevealCard
+        text="How can i Help you"
+        revealText="hi Guy's "
+      >
+  </TextRevealCard>
+
 </BackgroundLines>
 
   );
---
+++ src/components/Text/TextUi.jsx
@@ -0,0 +1,20 @@
+// import React from "react";
+// import {
+//   TextRevealCard,
+//   TextRevealCardDescription,
+// } from "../../components/Ui/Text/TextUi";
+
+// export function TextRevealCardPreview() {
+//   return (
+//     (<div
+//       className="flex items-center justify-center bg-red-300 h-[40rem] rounded-2xl w-full">
+//       <TextRevealCard text="You know the business" revealText="I know the chemistry ">
+      
+//         <TextRevealCardDescription>
+//           This is a text reveal card. Hover over the card to reveal the hidden
+//           text.
+//         </TextRevealCardDescription>
+//       </TextRevealCard>
+//     </div>)
+//   );
+// }
