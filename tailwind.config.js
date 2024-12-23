fdbaa988a23d693ddb7f5ea3b9e467c996362144
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2024-12-23 10:35:42 +0530

    give updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,20 +11,16 @@ module.exports = {
     extend: {
       animation: {
         shimmer: "shimmer 2s linear infinite",
-        // You can extend other animations here if needed
+        // Extend other animations here if needed
       },
       keyframes: {
         shimmer: {
-          from: {
-            backgroundPosition: "0 0",
-          },
-          to: {
-            backgroundPosition: "-200% 0",
-          },
+          from: { backgroundPosition: "0 0" },
+          to: { backgroundPosition: "-200% 0" },
         },
         // Add more keyframes if required
       },
-      // You can extend other theme settings like colors, spacing, etc., here
+      // Extend other theme settings like colors, spacing, etc., here
     },
   },
   plugins: [
